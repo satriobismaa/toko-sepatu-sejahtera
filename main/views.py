4ead611dcc57bfb495b029d4b64290db294101b3
satriobismaa <bismazharfan@gmail.com>
2025-09-15 22:18:52 +0700

    Menambah ss postman

--- main/views.py
+++ main/views.py
@@ -56,7 +56,7 @@ def show_json(request):
 def show_json_by_id(request, product_id):
     try:
         product_item = Product.objects.get(pk=product_id)
-        json_data = serializers.serialize("json", product_item)
+        json_data = serializers.serialize("json", [product_item])
         return HttpResponse(json_data, content_type="application/json")    
     except Product.DoesNotExist:
         return HttpResponse(status=404)   
